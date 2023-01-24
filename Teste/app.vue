0f38c9fa0cc2502a12344326ba0f3d45e4b0679a
acacio90 <113463515+luizabasseto@users.noreply.github.com>
2023-01-24 16:46:45 -0300

    src

--- Teste/app.vue
+++ Teste/app.vue
@@ -33,15 +33,15 @@ export default {
 
 <template>
   <NuxtLayout>
-    <div class="font-sans font-bold text-green-900 bg-emerald-50 p-4">
+    <div  id="header" class="font-sans font-bold text-green-900 bg-emerald-50 p-4">
       <h1 class="text-4xl m-5">Cadastro e Login</h1>
     </div>
     <AppHeader />
     <v-content>
       <div class="flex row">
-        <div id="forms" class="w-1/2 h-1/2">
+        <div id="forms" class="h-1/2 w-1/2 flex justify-center items-center m-14">
           <form id="app" @submit="checkForm" method="post"
-            class="bg-emerald-100 text-green-900 border-lime-900 leading-8 p-6 flex">
+            class="bg-emerald-100 text-green-900 border-lime-900 leading-8 p-6 flex flex-col justify-center">
             <h2 class="font-mono text-lg">Cadastrar</h2>
             <div>
               <label for="name"><b><u>Nome</u>:</b></label>
@@ -70,12 +70,12 @@ export default {
               </div>
             </div>
             <button type="submit"
-              class="flex text-lg border-2 rounded-lg border-green-900 p-2.5 m-2.5"><b>Enviar</b></button>
+              class="flex text-lg border-2 rounded-lg border-green-900 p-2.5 m-2.5 justify-center"><b>Enviar</b></button>
           </form>
         </div>
-        <div id="forms">
+        <div class="flex justify-center items-center m-14">
           <form id="app" @submit="checkForm" method="post"
-            class="bg-emerald-100 text-green-900 border-lime-900 leading-8 p-6 flex">
+            class="bg-emerald-100 text-green-900 border-lime-900 leading-8 p-6 flex flex-col justify-center">
 
             <h2 class="font-mono text-lg">Entrar</h2>
             <div>
@@ -87,7 +87,7 @@ export default {
               <input v-model="password" type="password" name="senha" placeholder="Digite sua senha" required>
             </div>
             <button type="submit"
-              class="flex text-lg border-2 rounded-lg border-green-900 p-2.5 m-2.5"><b>Enviar</b></button>
+              class="flex text-lg border-2 rounded-lg border-green-900 p-2.5 m-2.5 justify-center"><b>Enviar</b></button>
           </form>
         </div>
       </div>
@@ -100,10 +100,6 @@ export default {
 </template>
 
 <style scoped>
-form {
-  flex-direction: column;
-  justify-content: center;
-}
 
 input {
   margin: 10px;
@@ -114,22 +110,35 @@ label {
   margin: 10px;
 }
 
-
-
-h2,
-h1 {
+h2,h1 {
   text-align: center;
   padding: 5px;
 }
 
-button {
-  justify-content: center;
+@media screen and (max-width: 480px) {
+  #header{
+    width: 100%;
+    height: 120px;
+  }
+  h1{
+    font-size: 26px;
+  }
+
+  v-content>div{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+
+  #forms{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 50%;
+    width: 70%;
+   }
+
 }
 
-#forms {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin: 50px;
-}
+
 </style>
